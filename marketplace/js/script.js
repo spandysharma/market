e9dca653052544c4aa220db6171fa4cccb682c7d
spandysharma <112590656+spandysharma@users.noreply.github.com>
2023-06-05 16:49:44 -0400

    Delete marketplace directory

--- marketplace/js/script.js
+++
@@ -1,8 +0,0 @@
-// This script is for generating unique ids.
-// Requires one-time installation of node and npm.
-import { v4 as uuidv4 } from "uuid.js";
-// const { v4: uuidv4 } = require('uuid');
-const productId = uuidv4();
-document.getElementById('productId').innerHTML = productId;
-
-// console.log(productId);
